1e81866a7a5d10b10bda0b1ba4602e9e0e8d371c
rpdeeksh <prabhudeekshith11@gmail.com>
2024-04-03 21:01:41 +0530

    second-commit

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -5,13 +5,15 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";  
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract DegenToken is ERC20, Ownable, ERC20Burnable {
+contract DegenToken is ERC20, Ownable, ERC20Burnable 
+{
 
     string public redeem_shop;
-    int256 cost;
+    int256 public cost;
     string internal error_m = "Only 3 items available in the shop";
 
-    constructor() ERC20("Degen", "DGN") {
+    constructor() ERC20("Degen", "DGN") Ownable() {
+
         redeem_shop = "You have the following items in your shop which you are able to redeem: 1.Bat, 2.Ball, 3.Stumps";
     }
 
@@ -19,22 +21,27 @@ contract DegenToken is ERC20, Ownable, ERC20Burnable {
             _mint(to, amount);
         }
 
+        function transferFrom(address from, address to, uint256 amount) public override returns (bool) {
+            _transfer(from, to, amount);   
+             return true;
+        }
+
         function redeem(uint256 item_serial) public payable{
         
         if(item_serial > 0 && item_serial < 4){
             revert(error_m);
         }
-        require(balanceOf(msg.sender) >= 1001);
-        _burn(msg.sender, (item_serial*7) %5  + 999);
+        require(balanceOf(msg.sender) >= 1);
+        _burn(msg.sender, (item_serial*7));
         }
 
         function ReturnCode(string memory _name) public payable  {
     if (keccak256(bytes(_name)) == keccak256(bytes("Bat"))) {
-        cost = (1*7) + 999
+        cost = 1*7;
     } else if (keccak256(bytes(_name)) == keccak256(bytes("Ball"))) {
-        cost = (2*7) + 999
+        cost = 2*7 ;
     } else if (keccak256(bytes(_name)) == keccak256(bytes("Stumps"))) {
-        cost = (3*7) + 999
+        cost = 3*7;
     } else {
         revert("Item doesn't exist.");
     }
